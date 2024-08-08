1b342d8dee06c82663fa5cd96c8bfe1ab9e0e98a
Sivaibala K <sivaibala85@gmail.com>
2024-08-09 02:24:15 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -8,7 +8,13 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <title>Pantry Pal</title>
+        <meta author="Sivaibala" />
+      </head>
+      <body className={inter.className}>{children}
+
+      </body>
     </html>
   );
 }
